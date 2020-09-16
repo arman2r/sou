1f35bf5687270b04ec4c07e73b7739d8cb54cafc
Armando Rubio <arubio@qvision.com.co>
2020-09-16 12:10:11 -0500

    menu responsive

--- js/header.js
+++ js/header.js
@@ -1,3 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var dropShared = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(dropShared, {
+        direction: 'bottom',
+        hoverEnabled: false
+    });
+})
+
 window.onscroll = function() { scrollFunction() };
 
 var header = document.getElementById("header");
@@ -8,4 +16,12 @@ function scrollFunction() {
     } else {
         header.classList.remove('sticky')
     }
+}
+
+var buttonMenu = document.getElementById('openMenu');
+
+var menu = document.getElementById('menu');
+
+function openMenu() {
+    menu.classList.toggle('open-menu')
 }
