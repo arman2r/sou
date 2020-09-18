e3a77e3cd1efd52ec7e4399d32e9e47d0429217a
Armando Rubio <arubio@qvision.com.co>
2020-09-18 00:44:51 -0500

    ajustes de diseño index, menu y footer

--- js/header.js
+++ js/header.js
@@ -1,27 +1,54 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var dropShared = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(dropShared, {
-        direction: 'bottom',
-        hoverEnabled: false
-    });
-})
+var dropShared = document.querySelectorAll('.fixed-action-btn');
+var instances = M.FloatingActionButton.init(dropShared, {
+    direction: 'bottom',
+    hoverEnabled: false
+});
+var drop = document.querySelectorAll('.dropdown-trigger');
+M.Dropdown.init(drop);
+
 
 window.onscroll = function() { scrollFunction() };
 
 var header = document.getElementById("header");
-
-function scrollFunction() {
-    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-        header.classList.add('sticky')
-    } else {
-        header.classList.remove('sticky')
-    }
-}
-
 var buttonMenu = document.getElementById('openMenu');
 
 var menu = document.getElementById('menu');
+var menuContent = document.getElementById('menu');
+var tabMenu = menuContent.querySelectorAll('.tabs')[0];
+var backdrop = document.querySelectorAll('.submenu')[0];
+var btnOpMenu = document.querySelectorAll('.menu-btn')[0];
+console.log(tabMenu)
+
+function scrollFunction() {
+    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+        header.classList.add('sticky');
+        menu.classList.remove('open-menu');
+        backdrop.classList.add('closebackdrop');
+        btnOpMenu.classList.remove('close-text-btn');
+        if (menuContent.classList.contains('show-submenu')) {
+            menuContent.classList.remove('show-submenu');
+            tabMenu.classList.remove('show-submenu');
+        }
+    } else {
+        header.classList.remove('sticky');
+    }
+}
+
+
 
 function openMenu() {
-    menu.classList.toggle('open-menu')
+    menu.classList.toggle('open-menu');
+    btnOpMenu.classList.toggle('close-text-btn');
+    console.log(this)
+    if (menuContent.classList.contains('show-submenu')) {
+        menuContent.classList.remove('show-submenu');
+        tabMenu.classList.remove('show-submenu')
+    }
+
+}
+
+function openSubMenu() {
+    menuContent.classList.add('show-submenu');
+    tabMenu.classList.add('show-submenu');
+    backdrop.classList.remove('closebackdrop')
 }
